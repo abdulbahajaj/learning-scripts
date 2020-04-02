db3bc994c370a798513c269b0dd5abc723f081c4
Abdul Bahajaj <abdulbahajaj@gmail.com>
2020-04-02 11:11:32 +0000

    Use vars

--- repl.sh
+++ repl.sh
@@ -1,36 +1,43 @@
-# Delete netns0 if it exists
-sudo ip netns delete netns0
+export NAMESPACE=netns0;
+export NSVETH=veth-$NAMESPACE;
+export DEFVETH=veth-def$NAMESPACE;
+export NSIP=172.0.0.2
+export DEFIP=172.0.0.1
+
+# Delete namespace if it exists
+sudo ip netns delete $NAMESPACE
 
 # Setting up the namespace
-sudo ip netns add netns0
+sudo ip netns add $NAMESPACE
 echo "Added netns"
 
-sudo ip link add veth-default type veth peer name veth-netns0
+sudo ip link add $DEFVETH type veth peer name $NSVETH
 echo "Added veth network interfaces"
 
-sudo ip link set veth-netns0 netns netns0
+sudo ip link set $NSVETH netns $NAMESPACE
 echo "Moved one of the Veth pairs to the NETNS"
 
 # Bringing interfaces up
-sudo ip link set veth-default up
-sudo ip netns exec netns0 ip link set veth-netns0 up
+sudo ip link set $DEFVETH up
+sudo ip netns exec $NAMESPACE ip link set $NSVETH up
 echo "Brought the interfaces up"
 
 # Adding routing table
-sudo ip addr add 172.0.0.1/16 dev veth-default
-sudo ip netns exec netns0 ip addr add 172.0.0.2/16 dev veth-netns0
-sudo ip netns exec netns0 ip route add default via 172.0.0.1
+sudo ip addr add $DEFIP/16 dev $DEFVETH
+sudo ip netns exec $NAMESPACE ip addr add $NSIP/16 dev $NSVETH
+sudo ip netns exec $NAMESPACE ip route add default via $DEFIP
 echo "Added routing rules"
 
-Enable packet forwarding
+# Enable packet forwarding
 sudo sysctl net.ipv4.ip_forward=1
-echo "Enabled port forwarding"
+echo "Enabled ip forwarding"
 
 # Forward packet between veth-default and the exposed port
-sudo iptables -A FORWARD -o eth0 -i veth-default -j ACCEPT
-sudo iptables -A FORWARD -i eth0 -o veth-default -j ACCEPT
+sudo iptables -A FORWARD -o eth0 -i $DEFVETH -j ACCEPT
+sudo iptables -A FORWARD -i eth0 -o $DEFVETH -j ACCEPT
 echo "Set up iptable forwarding rules"
 
+# exit 1
 # Setting up  of masquerading IPs generated from the netns and heading to the internet
-sudo iptables -t nat -A POSTROUTING -s 172.0.0.2 -o eth0 -j MASQUERADE
+sudo iptables -t nat -A POSTROUTING -s $NSIP -o eth0 -j MASQUERADE
 echo "Set up masquerading"
